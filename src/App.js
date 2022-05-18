cf0be611b518768be7f7adef0e507d786569c48c
FannySundlofSopra <fanny.sundlof@soprasteria.com>
2022-05-18 18:32:11 +0200

    update

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ const contentfulCDA = require("contentful");
 const contentfulCMA = require('contentful-management')
 
 export const CDAclient = contentfulCDA.createClient({
-  space: '43o547dfys9y',
-  environment: 'fanny', // defaults to 'master' if not set
-  accessToken: 'sjWlleHTWjJsfXBFvEd41HS9NfKdBw4QjW-ixQLdE0Q'
+  space: '',
+  environment: '', // defaults to 'master' if not set
+  accessToken: ''
 })
 
 export const CMAclient = contentfulCMA.createClient({
   // This is the access token for this space. Normally you get the token in the Contentful web app
-  accessToken: 'CFPAT-QTQmBS2vubwsudLrTi2p6zGQxx9oQTWJtJoVfIYDsA0'
+  accessToken: ''
 })
 
 
