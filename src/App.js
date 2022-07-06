5515afb55b8d76094c982aaa582dd86becec3a1d
FannySundlofSopra <fanny.sundlof@soprasteria.com>
2022-07-06 13:28:18 +0200

    Removed config

--- src/App.js
+++ src/App.js
@@ -3,19 +3,7 @@ import './App.css';
 
 
 // CONTENTFUL CONFIG 
-const contentfulCDA = require("contentful");
-const contentfulCMA = require('contentful-management')
-
-export const CDAclient = contentfulCDA.createClient({
-  space: '',
-  environment: '', // defaults to 'master' if not set
-  accessToken: ''
-})
-
-export const CMAclient = contentfulCMA.createClient({
-  // This is the access token for this space. Normally you get the token in the Contentful web app
-  accessToken: ''
-})
+import {CDAclient, CMAclient} from "./config"
 
 
 function App() {
